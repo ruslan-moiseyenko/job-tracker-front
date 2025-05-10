a71b8ac44dc499c6e1c56d53439ed1590375b753
Ruslan Moiseyenko <ruslan@atomix-chat.com>
2025-05-10 11:38:26 +0300

    Add Eslint

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from "@eslint/js";
+import pluginRouter from "@tanstack/eslint-plugin-router";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  ...pluginRouter.configs["flat/recommended"],
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"]
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser }
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "react/jsx-filename-extension": [
+        "error",
+        {
+          extensions: [".jsx", ".tsx"]
+        }
+      ]
+    }
+  }
+]);
